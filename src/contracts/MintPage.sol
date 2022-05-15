b9e8d6c2582faa538e66ecf8fb30ef73c9040033
kazuma <kazuma.work77@gmail.com>
2022-05-15 17:48:13 +0900

    feat: contractの追加

--- src/contracts/MintPage.sol
+++ src/contracts/MintPage.sol
@@ -1,67 +1,80 @@
 // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.4;
 
-// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.4.2/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.4.2/contracts/utils/Counters.sol";
-// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.4.2/contracts/access/Ownable.sol";
-// import "https://github.com/Brechtpd/base64/blob/main/base64.sol";
+pragma solidity ^0.8.4;
 
-// contract mintPage is ERC721URIStorage, Ownable {
-//     using Counters for Counters.Counter;
-//     Counters.Counter private _tokenCounter;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.4.2/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.4.2/contracts/utils/Counters.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.4.2/contracts/access/Ownable.sol";
+import "https://github.com/Brechtpd/base64/blob/main/base64.sol";
 
-//     constructor () ERC721 ("MintTest","MT") {}
+contract studyjoin is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenCounter;
 
-//     uint256 MAX_ITEM = 100;
-//     uint256 public constant price = 1000000000000000;
+    bool public revealFlg = false;
+    bool public saleFlg = false;
+    //bytes public secretURL = "";
+    //bytes public baseURL = "https://gateway.pinata.cloud/ipfs/QmUJTnUaF28dvVj6xWKfywhRhx7WKhU4aHm1BdUb3HxUQc";
 
-//     mapping(address => uint256[]) private _addressToIds;
+    mapping(address => uint256) private _addressToApprove;
 
-//     //return metadata
-//     function tokenURI(uint256 _tokenId) public view virtual override returns (string memory) {
+    constructor () ERC721 ("YumeWeb3Study","YWS") {}
 
-//         bytes memory json = abi.encodePacked(
-//             '{',
-//                 '"name": "MintPage #',
-//                 Strings.toString(_tokenId),
-//                 '",',
-//                 '"description": "",',
-//                 '"image": "https://gateway.pinata.cloud/ipfs/QmP5v6PBDJqBAarDfNGyQMPY4eXuyc5MEq1zpsXcySSNxk"',
-//             '}'
-//         );
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        // bytes memory url;
 
-//         bytes memory metadata = abi.encodePacked(
-//             "data:application/json;base64,",
-//             Base64.encode(bytes(json))
-//         );
+        // if(revealFlg){
+        //     url = secretURL;
+        // }
+        // else{
+        //     url = baseURL;
+        // }
 
-//         return (string(metadata));
-//     }
+        bytes memory json = abi.encodePacked(
+            '{',
+                '"name": "MintPage #',
+                Strings.toString(tokenId),
+                '",',
+                '"description": "",',
+                '"image": "https://gateway.pinata.cloud/ipfs/QmUJTnUaF28dvVj6xWKfywhRhx7WKhU4aHm1BdUb3HxUQc"',
+            '}'
+        );
 
-//     //get information
-//     function getToken(address _owner)public view returns(uint256){
-//         uint256 numOfToken = _addressToIds[_owner].length;
-//         return numOfToken;
-//     }
+        bytes memory metadata = abi.encodePacked(
+            "data:application/json;base64,",
+            Base64.encode(bytes(json))
+        );
 
-//     function getSupply()public view returns(uint256){
-//         return _tokenCounter.current();
-//     }
+        return (string(metadata));
+    }
 
-//     function buy() public payable {
-//         require(_tokenCounter.current() < MAX_ITEM,"Purchase would exceed max supply of NFTs");
-//         // require(price <= msg.value, "Ether value sent is not correct");
+    function mint() public {
+        require(saleFlg);
+        require(_addressToApprove[_msgSender()] == 0);
 
-//         _tokenCounter.increment();
-//         uint256 newItemId = _tokenCounter.current();
-//         _safeMint(msg.sender, newItemId);
-//         _setTokenURI(newItemId, tokenURI(newItemId));
-//         _addressToIds[msg.sender].push(newItemId);
-//     }
+        _tokenCounter.increment();
+        uint256 newItemId = _tokenCounter.current();
 
-//     function withdraw() public onlyOwner {
-//         uint256 balance = address(this).balance;
-//         payable(msg.sender).transfer(balance);
-//     }
+        _safeMint(_msgSender(), newItemId);
+        _setTokenURI(newItemId, tokenURI(newItemId));
 
-// }
+        _addressToApprove[_msgSender()] = 1;
+    }
+
+    function changeSaleFlg() public onlyOwner {
+        saleFlg = true;
+    }
+
+    function changeRevealFlg() public onlyOwner {
+        revealFlg = true;
+    }
+
+    function getAddressApprove() public view returns(uint256){
+        return _addressToApprove[_msgSender()];
+    }
+
+    function getSupply()public view returns(uint256){
+        return _tokenCounter.current();
+    }
+
+}
